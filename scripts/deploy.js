a1bdffb3603fd1733d1e76f8e3e179de1f596d9e
bengillitt <bengillitt@gmail.com>
2023-08-27 15:03:24 +0100

    finished hardhat simple storage

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,105 @@
+// // imports
+// const { ethers, run, network } = require("hardhat")
+// require("dotenv").config()
+
+// // async main
+// async function main() {
+//     const SimpleStorageFactory =
+//         await ethers.getContractFactory("SimpleStorage")
+//     console.log("Deploying contract...")
+//     const simpleStorage = await SimpleStorageFactory.deploy()
+//     await simpleStorage.waitForDeployment()
+//     const contractAddress = await simpleStorage.getAddress()
+//     console.log(`Deployed contract to: ${contractAddress}`)
+//     // what happens when we deploy to our hardhat network?
+//     if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+//         console.log("Waiting for block txes...")
+//         await simpleStorage.deployTransaction
+//         await verify(simpleStorage.address, [])
+//     }
+
+//     const currentValue = await simpleStorage.retrieve()
+//     console.log(`Current Value is: ${currentValue}`)
+
+//     // Update the current value
+//     const transactionResponse = await simpleStorage.store(7)
+//     await transactionResponse.wait(1)
+//     const updatedValue = await simpleStorage.retrieve()
+//     console.log(`Updated Value is: ${updatedValue}`)
+// }
+
+// async function verify(contractAddress, args) {
+//     console.log("Verifying contract...")
+//     try {
+//         await run("verify:verify", {
+//             address: contractAddress,
+//             constructorArguments: args,
+//         })
+//     } catch (e) {
+//         if (e.message.toLowerCase().includes("already verified")) {
+//             console.log("Already Verified!")
+//         } else {
+//             console.log(e)
+//         }
+//     }
+// }
+
+// // main
+// main()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//         console.log(error)
+//         process.exit(1)
+//     })
+
 // imports
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
-    const SimpleStorageFactory = await ethers.getContractFactory(
-      "SimpleStorage"
-    )
-      console.log("Deploying contract...")
+    const SimpleStorageFactory =
+        await ethers.getContractFactory("SimpleStorage")
+    console.log("Deploying contract...")
     const simpleStorage = await SimpleStorageFactory.deploy()
-    await simpleStorage.waitForDeployment();
-    const contractAddress = await simpleStorage.getAddress()
-    console.log(`Deployed contract to: ${contractAddress}`);
+    console.log(`Deployed contract to: ${simpleStorage.target}`)
+    // what happens when we deploy to our hardhat network?
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...")
+        await simpleStorage.deploymentTransaction().wait(6)
+        await verify(simpleStorage.target, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
 }
 
-async function verify(contractAddress, args) {
-  
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 // main
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
